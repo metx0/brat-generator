a2b8ae8cc2eabc2fbf9a6205ffb012c0fd157f82
miguel <migtorruco@gmail.com>
2024-06-20 00:06:31 -0600

    container sizing fixed decently

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const input = document.querySelector('.textInput')
-const content = document.querySelector('.content')
+const input = document.querySelector(".textInput");
+const content = document.querySelector(".content");
 
-content.textContent = input.value
+content.textContent = input.value;
+const contentStyles = window.getComputedStyle(content);
+const originalFontSize = contentStyles.getPropertyValue("font-size");
 
-input.addEventListener('input', (event) => {
-    content.textContent = event.target.value
-})
+const decrement = 15;
+
+input.addEventListener("input", (event) => {
+	content.textContent = event.target.value;
+
+	if (content.textContent.length < 35) {
+		content.style.fontSize = originalFontSize;
+	}
+
+	if (content.scrollHeight > content.clientHeight) {
+		adjustFontSize();
+	}
+});
+
+function adjustFontSize() {
+	let fontSize = parseInt(contentStyles.getPropertyValue("font-size"));
+	let newFontSize = fontSize - decrement;
+	content.style.fontSize = newFontSize + "px";
+}
